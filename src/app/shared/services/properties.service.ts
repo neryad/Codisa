18511ebdcdf902eeee5e811b3dcce166e404082d
neryad <dayern.gomez@hotmail>
2022-07-13 15:59:28 -0400

    init of services

--- src/app/shared/services/properties.service.ts
+++ src/app/shared/services/properties.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -7,5 +9,14 @@ import { Injectable } from '@angular/core';
 export class PropertiesService {
   //properties;
   //images/6140f2528e048453ea123c98
-  constructor(private http:HttpClient) {}
+  constructor(private http: HttpClient) {}
+
+  getProperties():Observable<any> {
+    return this.http.get(`${environment.apiUrl}properties`);
+  }
+
+  getPropertiesImages(id:string):Observable<any>{
+    return this.http.get(`${environment.apiUrl}images/${id}`);
+  }
 }
+
