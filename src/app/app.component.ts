9240c68f6ce0c0c61d1cb2b136af6788bf974311
neryad <dayern.gomez@hotmail>
2022-07-13 23:51:07 -0400

    beta-final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/internal/operators/map';
+
 import { Properties, Property } from './shared/interfaces/properties.interface';
 import { PropertiesService } from './shared/services/properties.service';
 
@@ -11,13 +13,52 @@ export class AppComponent implements OnInit {
   title = 'Codisa';
   constructor(private propertiesServices: PropertiesService) {}
   ngOnInit(): void {
-    this.getProperties();
+    this.getProperties(this.currentProperty);
   }
-  properties!: Property;
-  getProperties() {
-    this.propertiesServices.getProperties().subscribe((res: any) => {
-      this.properties = res.properties[0];
-      console.log(this.properties);
+  properties: Properties[] = [];
+  property: any;
+  currentPropertyImg = '';
+  currentProperty: number = 0;
+
+  getProperties(index: number) {
+    try {
+      this.propertiesServices.getProperties().subscribe((res: any) => {
+         this.properties = res.properties;
+         this.property = res.properties[index];
+
+
+         this.getPropertyImage(res.properties[index]._id);
+      });
+    } catch (error) {
+      console.log(error);
+      return error;
+    }
+  }
+
+  getPropertyImage(id: string) {
+    return this.propertiesServices.getPropertiesImages(id).subscribe((res) => {
+      this.currentPropertyImg = res.images[0];
+
     });
+  }
+
+  nextProperty(index: number) {
+console.log(index, 'index');
+  this.currentProperty++;
+  index++;
+
+    if (index == this.properties.length) {
+      console.log('weo');
+
+      this.currentProperty = 0;
+      this.getProperties(this.currentProperty);
+      return;
+    }
+    this.getProperties(this.currentProperty);
+
+  }
+  viewOnMap(property: Property) {
+    let mapUrl = `https://www.google.com/maps/search/?api=1&query=${property.location.latitude},${property.location.longitude}`;
+    window.open(mapUrl, '_blank');
   }
 }
--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -3,17 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css', '../../../../styles.css']
+  styleUrls: ['./navbar.component.css', '../../../../styles.css'],
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
-  onSales = ['Casas', 'Apartamentos', 'Oficinas', 'Desarrollos' ,'nuevos'];
+  constructor() {}
+  onSales = ['Casas', 'Apartamentos', 'Oficinas', 'Desarrollos', 'nuevos'];
   onRents = ['Brokers', 'Dueños'];
-  mortgages= ['Entidades financieras', 'Buscar preaprobación']
-  needHelp = ['Guías para comprar una propiedad']
+  mortgages = ['Entidades financieras', 'Buscar preaprobación'];
+  needHelp = ['Guías para comprar una propiedad'];
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
